231e4124e9dd77185f466cfbc020de2a59157540
MarkRamos98 <marcos.ramos123@live.com>
2025-04-07 22:07:09 -0300

    teste

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,6 @@ Route::get('/contato', [App\Http\Controllers\ContatoController::class, 'contato'
 
 Route::get('/sobre-nos', [App\Http\Controllers\SObreNosController::class, 'sobreNos'])->name('site.sobre-nos');
 
-Route::get('/fornecedores', [App\Http\Controllers\FornecedoresController::class, 'fornecedores']);
-
-Route::get('/servicos', [App\Http\Controllers\ServicosController::class, 'servicos']);
-
 Route::prefix("/api")->group(function(){
     Route::get('/login', function(){
         return 'login';
